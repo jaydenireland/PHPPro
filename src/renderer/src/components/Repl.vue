5296e607a25950a6cf83a5da401fe69646c2cea3
Jayden Ireland <jayden@hydracor.net>
2024-05-27 12:59:49 +0200

    Add debounce

--- src/renderer/src/components/Repl.vue
+++ src/renderer/src/components/Repl.vue
@@ -1,23 +1,27 @@
 <script setup lang="ts">
 import { ref, watch } from 'vue'
+import _ from 'lodash'
 import Editor from './Editor.vue'
 import OutputRender from './OutputRender.vue'
 
 const code = ref<string>('')
 const output = ref<string>('')
 
-watch(code, async (newVal) => {
-  try {
-    const val = await window.electron.ipcRenderer.invoke('code', newVal)
-    if (val instanceof Error) {
-      output.value = val.message
-      return
+watch(
+  code,
+  _.debounce(async (newVal) => {
+    try {
+      const val = await window.electron.ipcRenderer.invoke('code', newVal)
+      if (val instanceof Error) {
+        output.value = val.message
+        return
+      }
+      output.value = val
+    } catch (e) {
+      output.value = (e as Error).message
     }
-    output.value = val
-  } catch (e) {
-    output.value = (e as Error).message
-  }
-})
+  }, 300)
+)
 </script>
 
 <template>
